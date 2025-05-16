6873522715cba3c17a349dd09d146d01fcee6009
GRD39101 <grobertdeaniii@gmail.com>
2025-05-15 23:49:03 -0400

    added in question input section

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title('George Dean III Workout Tracker Dashboard')
+question = st.text_input("Enter the workouts for today: ")
+if(st.button('Submit')):
+    result = question.title()
+    st.success(result)
+
